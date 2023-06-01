d0b0c939f225fb6122b521f860b814de6a009f04
neilcantorne <cantorne.neil@gmail.com>
2023-06-01 20:07:03 +0800

    add AuthMiddleware

--- src/routers/Service.ts
+++ src/routers/Service.ts
@@ -1,46 +1,46 @@
-import express from 'express';
-import { Service } from '../entity/Service';
-import { dataSource } from '../data-source';
-import _ from 'lodash';
+import express from "express";
+import { Service } from "../entity/Service";
+import { dataSource } from "../data-source";
+import _ from "lodash";
+import authMiddleware from "./AuthMiddleware";
 const router = express.Router();
 
-router.get('/services', async (req, res) => {
-    const repository = dataSource.getRepository(Service)
-    const services = await repository.find()
-    res.json(services)
-})
+router.get("/services", authMiddleware, async (req, res) => {
+  const repository = dataSource.getRepository(Service);
+  const services = await repository.find();
+  res.json(services);
+});
 
-router.get('/service/:id', async (req, res) => {
-    const repository = dataSource.getRepository(Service)
-    const service = await repository.findOneByOrFail({ id: req?.params?.id })
-    res.json(service)
-})
+router.get("/service/:id", authMiddleware, async (req, res) => {
+  const repository = dataSource.getRepository(Service);
+  const service = await repository.findOneByOrFail({ id: req?.params?.id });
+  res.json(service);
+});
 
-router.post('/add/service', async (req, res) => {
-    const repository = dataSource.getRepository(Service)
-    let service = req?.body
+router.post("/add/service", authMiddleware, async (req, res) => {
+  const repository = dataSource.getRepository(Service);
+  let service = req?.body;
 
-    service.name = _.startCase(service.name)
-    service.category = _.lowerCase(service.category)
+  service.name = _.startCase(service.name);
+  service.category = _.lowerCase(service.category);
 
-    service = repository.create(service)
-    service = repository.save(service)
-    return res.send(service)
-})
+  service = repository.create(service);
+  service = repository.save(service);
+  return res.send(service);
+});
 
-router.put('/update/service/:id', async (req, res) => {
-    const repository = dataSource.getRepository(Service)
-    let service = await repository.findOneByOrFail({ id: req?.params?.id })
-    repository.merge(service, req?.body)
-    repository.save(service)
-    return res.send(service)
-})
+router.put("/update/service/:id", authMiddleware, async (req, res) => {
+  const repository = dataSource.getRepository(Service);
+  let service = await repository.findOneByOrFail({ id: req?.params?.id });
+  repository.merge(service, req?.body);
+  repository.save(service);
+  return res.send(service);
+});
 
-router.delete('/delete/service/:id', async (req, res) => {
-    const repository = dataSource.getRepository(Service)
-    let service = await repository.delete(req?.params?.id)
-    return res.send(service)
-})
- 
+router.delete("/delete/service/:id", authMiddleware, async (req, res) => {
+  const repository = dataSource.getRepository(Service);
+  let service = await repository.delete(req?.params?.id);
+  return res.send(service);
+});
 
-export default router
+export default router;
